bf5e01b401327f9d14e2a634b66460952d42c284
DCwza <119536260+DCwza@users.noreply.github.com>
2023-03-03 13:06:43 -0800

    aa

--- index.js
+++ index.js
@@ -4,18 +4,18 @@ app.use('/', express.static('public'));
 const PORT = process.env.PORT || 3000
 app.set('view engine', 'pug');
 
-// app.get('/:image', (req, res) => {
-//     res.render('imagerender', { image: req.params.image});
-// });
+app.get('/:image', (req, res) => {
+     res.render('imagerender', { image: req.params.image});
+ });
 
-// app.get('/', (req, res) => {
+//app.get('/', (req, res) => {
 //     res.render('imagerender', {image: "img1.jpg"})
 // })
 
-app.get('/:image', (req, res) => {
-    imageURL = req.params.image + ".jpg"
-    res.render('imagerender', {image: imageURL})
-})
+//app.get('/:image', (req, res) => {
+//    imageURL = req.params.image + ".jpg"
+//    res.render('imagerender', {image: imageURL})
+//})
 
 
 app.listen(PORT, () => {
